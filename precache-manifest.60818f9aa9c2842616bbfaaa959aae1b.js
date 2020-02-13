4a50c8ed9b4eb62990be0e86d323e1aa1f7d050b
Bogdan <AlenWhalker@gmail.com>
2020-02-13 11:32:44 +0300

    iniitial commit

---
+++ precache-manifest.60818f9aa9c2842616bbfaaa959aae1b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1b9ec639b8fdf6651627f09f767c76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aad0477f060e560dce3",
+    "url": "/static/css/2.c26f5790.chunk.css"
+  },
+  {
+    "revision": "d96d4ef747dfaa9ed9d6",
+    "url": "/static/css/main.269444bf.chunk.css"
+  },
+  {
+    "revision": "2aad0477f060e560dce3",
+    "url": "/static/js/2.c16c4bc5.chunk.js"
+  },
+  {
+    "revision": "d96d4ef747dfaa9ed9d6",
+    "url": "/static/js/main.f7064b27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cbd8a51359aa48e8480c23911284bf24",
+    "url": "/static/media/Loader.cbd8a513.gif"
+  },
+  {
+    "revision": "cf3ad845ac921c8d03ddd28a1ab5cd41",
+    "url": "/static/media/Web.cf3ad845.svg"
+  },
+  {
+    "revision": "8def345867d86571b981fc065c98d7d1",
+    "url": "/static/media/behance-logo.8def3458.svg"
+  },
+  {
+    "revision": "5d4fc34e31a352a43f6b86d504f7dfbd",
+    "url": "/static/media/btcu.5d4fc34e.svg"
+  },
+  {
+    "revision": "c6530a2f0d0b52aa958bf52e42de2ed9",
+    "url": "/static/media/contacts-picture.c6530a2f.jpg"
+  },
+  {
+    "revision": "8aeea01c0629343b7351d0c1470a1b85",
+    "url": "/static/media/digital.8aeea01c.svg"
+  },
+  {
+    "revision": "fe30dd78d7b446c556cbc5361e1878a1",
+    "url": "/static/media/facebook-logo.fe30dd78.svg"
+  },
+  {
+    "revision": "30bb825bbad6618f4280cc431cd7ac47",
+    "url": "/static/media/first-screen.30bb825b.png"
+  },
+  {
+    "revision": "2229a428f778a3919da07ae58be6e1c8",
+    "url": "/static/media/icomoon.2229a428.woff"
+  },
+  {
+    "revision": "70fbb832698a05c2996ddcb92b05b935",
+    "url": "/static/media/icomoon.70fbb832.svg"
+  },
+  {
+    "revision": "f01c0403326526eed539cbd67d23a538",
+    "url": "/static/media/icomoon.f01c0403.ttf"
+  },
+  {
+    "revision": "f45cd78863f62cabf1c796ca131456a8",
+    "url": "/static/media/icomoon.f45cd788.eot"
+  },
+  {
+    "revision": "9dcd14d2697f6d1a838e56fe663ecd35",
+    "url": "/static/media/instagram-logo.9dcd14d2.svg"
+  },
+  {
+    "revision": "e235ba66201124adf51f69882d2c2086",
+    "url": "/static/media/jambo.e235ba66.svg"
+  },
+  {
+    "revision": "14400627364d22b383aff5cbdd4b9d9b",
+    "url": "/static/media/lacto.14400627.png"
+  },
+  {
+    "revision": "b9bb8b8548dfef05314be872a6282837",
+    "url": "/static/media/linkedin-logo.b9bb8b85.svg"
+  },
+  {
+    "revision": "4077462d6ba715d9a62a0e99555e1dfb",
+    "url": "/static/media/mobile-app.4077462d.svg"
+  },
+  {
+    "revision": "36bbc1309eda0420ded5f27805e40122",
+    "url": "/static/media/pigeon.36bbc130.svg"
+  },
+  {
+    "revision": "b7e50f7da4f92a72ff83da2e8117423b",
+    "url": "/static/media/sbf.b7e50f7d.png"
+  },
+  {
+    "revision": "2523416c93c0ea68da7ce1a2a6b10034",
+    "url": "/static/media/second-screen.2523416c.png"
+  }
+]);
